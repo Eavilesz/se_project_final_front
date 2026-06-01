1f86eb4783e92b6a23283722ff3e1d81b11b178d
Joe Roodvoets <jroodvoets6@gmail.com>
2026-06-01 17:53:36 -0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from "./App.jsx";
 import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 );
